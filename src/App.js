67e47951313d851fe51c90ad7c9b75e67f7b4ddd
Aman kukna <aman.kukna.007@gmail.com>
2024-05-13 20:39:08 +0530

    new project
    poster

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <div className='poster-box'>
+    <div className='image-div'>
+
+    </div>
+    <div className='detail-div'>
+    <h1 className='h1' >Home Made Food</h1>
+    <p className='p'>Healthy and Tasty</p>
+    <div className='enquery'>
+      <button>Enquery Now</button>
+      <p className='number'>9772201314</p>
+      <p className='address'>Ward no.09 near public park </p>
+
+    </div>
+    </div>
+
+  </div>
+  </>
+  );
+}
+
+export default App;
